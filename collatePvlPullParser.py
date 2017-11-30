f96b811969d2228b8f2fc7dd79ca483a682d12db
David J Birnbaum <djbpitt@gmail.com>
2017-11-30 15:51:05 -0500

    reran collation with new near-matching code (from ronald's nearmatching branch)

--- collatePvlPullParser.py
+++ collatePvlPullParser.py
@@ -21,7 +21,7 @@
 # Inline elements (with content) retained in normalization: pageRef, sup, sub, choice, option
 
 import sys
-# sys.path.insert(0, '/Users/djb/collatex/collatex-pythonport')  # CollateX from repo if not installed
+sys.path.insert(0, '/Users/djb/collatex/collatex-pythonport')  # CollateX from repo if not installed
 from collatex import *
 from xml.dom import pulldom
 import string
@@ -163,7 +163,7 @@ def extract(input_xml):
             print(n + ' input:\n')
             print(jsonInput)
             print(n + ' output:\n')
-            table = collate(rdgs, segmentation=False, near_match=True)
+            table = collate(rdgs, segmentation=False, near_match=False)
             outputFile.write('\n' + n + '\n' + str(table))
             print(table)
         # empty inline elements: lb, pb
@@ -188,5 +188,5 @@ def extract(input_xml):
                 witnesses.append(processRdg(currentSiglum, tokenize(currentRdg)))
     return True
 
-with open('pvl.xml', 'rb') as inputFile, open('output_near.txt', 'w') as outputFile:
+with open('pvl.xml', 'rb') as inputFile, open('output_exact.txt', 'w') as outputFile:
     parseResult = extract(inputFile)
